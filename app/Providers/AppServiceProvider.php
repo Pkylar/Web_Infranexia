cea11bdd94c3f96134b33c957c335ec3671c0094
Pkylar <purindakylalarissa@gmail.com>
2025-09-08 16:02:49 +0700

    fix: force https, trust proxies, set compiled views path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,8 +11,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+         if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+     }
     }
 }
